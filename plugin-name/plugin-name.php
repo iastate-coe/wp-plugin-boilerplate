07b0bd4746d92449431f2ace8f03d5b8b0a7e9e6
Kevin Wickham <kwickham@iastate.edu>
2020-12-09 16:27:02 -0600

    updated defaults with ISU info

--- plugin-name/plugin-name.php
+++ plugin-name/plugin-name.php
@@ -8,19 +8,19 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              http://example.com
+ * @link              http://iastate.edu
  * @since             1.0.0
  * @package           Plugin_Name
  *
  * @wordpress-plugin
  * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/plugin-name-uri/
+ * Plugin URI:        http://iastate.edu/plugin-name-uri/
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
  * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Author URI:        http://iastate.edu/
+ * License:           Iowa State University of Science and Technology
+ * License URI:       https://www.policy.iastate.edu/softwarecopyright
  * Text Domain:       plugin-name
  * Domain Path:       /languages
  */
